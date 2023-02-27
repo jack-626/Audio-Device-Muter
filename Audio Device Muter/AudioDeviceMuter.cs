603994c02e0f5fcd295d9d4d525b43c885ab6396
Jack <86790957+jack-626@users.noreply.github.com>
2023-02-27 18:51:40 +0000

    v2.0
    added auto toggle on startup and cleaned up the code a bit.

---
+++ Audio Device Muter/AudioDeviceMuter.cs
@@ -0,0 +1,93 @@
+using NAudio;
+using System.Diagnostics;
+
+namespace Audio_Device_Muter
+{
+    public partial class AudioDeviceMuter : Form
+    {
+        public NAudio.CoreAudioApi.MMDeviceCollection devices;
+        public AudioDeviceMuter()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a device!", "Error");
+                return;
+            }
+
+            var currentItem = comboBox1.SelectedItem;
+
+            foreach (NAudio.CoreAudioApi.MMDevice dev in devices)
+            {
+                if (currentItem.ToString() == dev.FriendlyName)
+                {
+                    var muted = dev.AudioEndpointVolume.Mute;
+                    muted = !muted;
+                    dev.AudioEndpointVolume.Mute = muted;
+                    switch (muted)
+                    {
+                        case true:
+                            MessageBox.Show("Muted Device!", "Success!");
+                            break;
+                        case false:
+                            MessageBox.Show("Unmuted Device!", "Success!");
+                            break;
+                    }
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a device!", "Error");
+                return;
+            }
+
+            var currentItem = comboBox2.SelectedItem;
+
+            TextWriter tw = new StreamWriter("audiodevicesave.txt");
+            tw.Write(currentItem.ToString());
+            tw.Close();
+        }
+        private void AudioDeviceMuter_Load(object sender, EventArgs e)
+        {
+            NAudio.CoreAudioApi.MMDeviceEnumerator enumerator = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+            devices = enumerator.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.Active);
+            comboBox1.Items.AddRange(devices.ToArray());
+            comboBox2.Items.AddRange(devices.ToArray());
+
+            //Check if device should be toggled and which one, then toggle it on startup.
+            if (File.Exists("audiodevicesave.txt"))
+            {
+                TextReader tr = new StreamReader("audiodevicesave.txt");
+                string deviceToToggle = tr.ReadToEnd();
+                Debug.WriteLine(deviceToToggle);
+                tr.Close();
+
+                foreach (NAudio.CoreAudioApi.MMDevice device in devices)
+                {
+                    if (device.FriendlyName == deviceToToggle)
+                    {
+                        var muted = device.AudioEndpointVolume.Mute;
+                        muted = !muted;
+                        device.AudioEndpointVolume.Mute = muted;
+                        switch (muted)
+                        {
+                            case true:
+                                MessageBox.Show("Muted Device!", "Success!");
+                                break;
+                            case false:
+                                MessageBox.Show("Unmuted Device!", "Success!");
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
