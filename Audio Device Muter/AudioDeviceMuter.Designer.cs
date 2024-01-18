8dcf4627bd46a32aa41ed61d23e2ad3ac3d3237f
Jack <86790957+jack-626@users.noreply.github.com>
2024-01-18 03:38:48 +0000

    v3.0
    - Combined the two buttons and dropdowns. One button now saves and toggles mute.
    - Added new dialog box that asks if you want to change saved device on start-up, if save already exists.

--- Audio Device Muter/AudioDeviceMuter.Designer.cs
+++ Audio Device Muter/AudioDeviceMuter.Designer.cs
@@ -29,95 +29,59 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AudioDeviceMuter));
-            this.deviceSelectBox1 = new System.Windows.Forms.ComboBox();
-            this.toggleButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.saveButton = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.deviceSelectBox2 = new System.Windows.Forms.ComboBox();
-            this.SuspendLayout();
+            deviceSelectBox1 = new ComboBox();
+            toggleButton = new Button();
+            label1 = new Label();
+            SuspendLayout();
             // 
             // deviceSelectBox1
             // 
-            this.deviceSelectBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.deviceSelectBox1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.deviceSelectBox1.FormattingEnabled = true;
-            this.deviceSelectBox1.Location = new System.Drawing.Point(12, 26);
-            this.deviceSelectBox1.Name = "deviceSelectBox1";
-            this.deviceSelectBox1.Size = new System.Drawing.Size(291, 22);
-            this.deviceSelectBox1.TabIndex = 0;
+            deviceSelectBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            deviceSelectBox1.Font = new Font("Verdana", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            deviceSelectBox1.FormattingEnabled = true;
+            deviceSelectBox1.Location = new Point(12, 26);
+            deviceSelectBox1.Name = "deviceSelectBox1";
+            deviceSelectBox1.Size = new Size(291, 22);
+            deviceSelectBox1.TabIndex = 0;
             // 
             // toggleButton
             // 
-            this.toggleButton.Location = new System.Drawing.Point(309, 19);
-            this.toggleButton.Name = "toggleButton";
-            this.toggleButton.Size = new System.Drawing.Size(97, 34);
-            this.toggleButton.TabIndex = 1;
-            this.toggleButton.Text = "Toggle Mute";
-            this.toggleButton.UseVisualStyleBackColor = true;
-            this.toggleButton.Click += new System.EventHandler(this.toggleButton_Click);
+            toggleButton.Location = new Point(309, 19);
+            toggleButton.Name = "toggleButton";
+            toggleButton.Size = new Size(150, 34);
+            toggleButton.TabIndex = 1;
+            toggleButton.Text = "Save & Toggle Mute";
+            toggleButton.UseMnemonic = false;
+            toggleButton.UseVisualStyleBackColor = true;
+            toggleButton.Click += toggleButton_Click;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Verdana", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 14);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Select a Device";
-            // 
-            // saveButton
-            // 
-            this.saveButton.Location = new System.Drawing.Point(309, 61);
-            this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(97, 34);
-            this.saveButton.TabIndex = 3;
-            this.saveButton.Text = "Save";
-            this.saveButton.UseVisualStyleBackColor = true;
-            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Verdana", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(12, 51);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(240, 14);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Select a device to toggle on startup";
-            // 
-            // deviceSelectBox2
-            // 
-            this.deviceSelectBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.deviceSelectBox2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.deviceSelectBox2.FormattingEnabled = true;
-            this.deviceSelectBox2.Location = new System.Drawing.Point(12, 68);
-            this.deviceSelectBox2.Name = "deviceSelectBox2";
-            this.deviceSelectBox2.Size = new System.Drawing.Size(291, 22);
-            this.deviceSelectBox2.TabIndex = 5;
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 9F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 14);
+            label1.TabIndex = 2;
+            label1.Text = "Select a Device";
             // 
             // AudioDeviceMuter
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(417, 106);
-            this.Controls.Add(this.deviceSelectBox2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.saveButton);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.toggleButton);
-            this.Controls.Add(this.deviceSelectBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "AudioDeviceMuter";
-            this.Text = "Audio Device Muter";
-            this.Load += new System.EventHandler(this.AudioDeviceMuter_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(469, 68);
+            Controls.Add(label1);
+            Controls.Add(toggleButton);
+            Controls.Add(deviceSelectBox1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "AudioDeviceMuter";
+            Text = "Audio Device Muter";
+            Load += AudioDeviceMuter_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -125,8 +89,5 @@
         private ComboBox deviceSelectBox1;
         private Button toggleButton;
         private Label label1;
-        private Button saveButton;
-        private Label label2;
-        private ComboBox deviceSelectBox2;
     }
 }
--- Audio Device Muter/AudioDeviceMuter.cs
+++ Audio Device Muter/AudioDeviceMuter.cs
@@ -11,16 +11,8 @@ namespace Audio_Device_Muter
             InitializeComponent();
         }
 
-        private void toggleButton_Click(object sender, EventArgs e)
+        private void MuteDevice(object currentItem, bool showWindow)
         {
-            if (deviceSelectBox1.SelectedItem == null)
-            {
-                MessageBox.Show("Please select a device!", "Error");
-                return;
-            }
-
-            var currentItem = deviceSelectBox1.SelectedItem;
-
             foreach (NAudio.CoreAudioApi.MMDevice dev in devices)
             {
                 if (currentItem.ToString() == dev.FriendlyName)
@@ -28,58 +20,8 @@ namespace Audio_Device_Muter
                     var muted = dev.AudioEndpointVolume.Mute;
                     muted = !muted;
                     dev.AudioEndpointVolume.Mute = muted;
-                    switch (muted)
+                    if (showWindow)
                     {
-                        case true:
-                            MessageBox.Show("Muted Device!", "Success!");
-                            break;
-                        case false:
-                            MessageBox.Show("Unmuted Device!", "Success!");
-                            break;
-                    }
-                }
-            }
-        }
-        private void saveButton_Click(object sender, EventArgs e)
-        {
-            if (deviceSelectBox2.SelectedItem == null)
-            {
-                MessageBox.Show("Please select a device!", "Error");
-                return;
-            }
-
-            var currentItem = deviceSelectBox2.SelectedItem;
-
-            TextWriter tw = new StreamWriter("audiodevicesave.txt");
-            tw.Write(currentItem.ToString());
-            tw.Close();
-            if (File.Exists("audiodevicesave.txt"))
-            {
-                MessageBox.Show("Saved Successfully!", "Success!");
-            }
-        }
-        private void AudioDeviceMuter_Load(object sender, EventArgs e)
-        {
-            NAudio.CoreAudioApi.MMDeviceEnumerator enumerator = new NAudio.CoreAudioApi.MMDeviceEnumerator();
-            devices = enumerator.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.Active);
-            deviceSelectBox1.Items.AddRange(devices.ToArray());
-            deviceSelectBox2.Items.AddRange(devices.ToArray());
-
-            //Check if device should be toggled and which one, then toggle it on startup.
-            if (File.Exists("audiodevicesave.txt"))
-            {
-                TextReader tr = new StreamReader("audiodevicesave.txt");
-                string deviceToToggle = tr.ReadToEnd();
-                Debug.WriteLine(deviceToToggle);
-                tr.Close();
-
-                foreach (NAudio.CoreAudioApi.MMDevice device in devices)
-                {
-                    if (device.FriendlyName == deviceToToggle)
-                    {
-                        var muted = device.AudioEndpointVolume.Mute;
-                        muted = !muted;
-                        device.AudioEndpointVolume.Mute = muted;
                         switch (muted)
                         {
                             case true:
@@ -90,6 +32,64 @@ namespace Audio_Device_Muter
                                 break;
                         }
                     }
+                }
+            }
+        }
+
+        private void toggleButton_Click(object sender, EventArgs e)
+        {
+
+            //No device selected
+            if (deviceSelectBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a device!", "Error");
+                return;
+            }
+            if (deviceSelectBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a device!", "Error");
+                return;
+            }
+
+            //Write current item to file
+            var currentItem = deviceSelectBox1.SelectedItem;
+
+            TextWriter tw = new StreamWriter("audiodevicesave.txt");
+            tw.Write(currentItem.ToString());
+            tw.Close();
+            if (!File.Exists("audiodevicesave.txt"))
+            {
+                MessageBox.Show("Saving failed.", "Error");
+            }
+
+            //Mute currently selected device
+            MuteDevice(deviceSelectBox1.SelectedItem, true);
+        }
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void AudioDeviceMuter_Load(object sender, EventArgs e)
+        {
+            NAudio.CoreAudioApi.MMDeviceEnumerator enumerator = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+            devices = enumerator.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.Active);
+            deviceSelectBox1.Items.AddRange(devices.ToArray());
+
+            if (File.Exists("audiodevicesave.txt"))
+            {
+                TextReader tr = new StreamReader("audiodevicesave.txt");
+                string deviceToToggle = tr.ReadToEnd();
+                Debug.WriteLine(deviceToToggle);
+                tr.Close();
+
+                MuteDevice(deviceToToggle, false);
+
+                //Check if device should be toggled and which one, then toggle it on startup.
+                DialogResult dialog = MessageBox.Show("Saved device muted. \nWould you like to change the saved device?", "Success!", MessageBoxButtons.YesNo);
+
+                if (dialog == DialogResult.No)
+                {
+                    Application.Exit();
                 }
             }
         }
